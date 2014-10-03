9aaea9c85a66062ca2c6acad23463469f0a61e4c
Florent gravin <florent.gravin@camptocamp.com>
2014-10-03 16:21:30 +0200

    Element boundedBy must not be set as geometry field on GML reading

--- src/ol/format/gml/gmlbase.js
+++ src/ol/format/gml/gmlbase.js
@@ -169,8 +169,11 @@ ol.format.GMLBase.prototype.readFeatureElement = function(node, objectStack) {
       }
       values[ol.xml.getLocalName(n)] = value;
     } else {
-      geometryName = ol.xml.getLocalName(n);
-      values[geometryName] = this.readGeometryElement(n, objectStack);
+      // boundedBy is an extent and must not be considered as a geometry
+      if (n.localName !== 'boundedBy') {
+        geometryName = ol.xml.getLocalName(n);
+      }
+      values[ol.xml.getLocalName(n)] = this.readGeometryElement(n, objectStack);
     }
   }
   var feature = new ol.Feature(values);
